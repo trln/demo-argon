5be248c320e09718cd607ee7a0f3bfe40eb8bee4
Genia Kazymova <genia@trln.org>
2022-07-14 14:04:19 +0000

    Update bl to v7.28

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,6 @@ module Argo
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.action_mailer.default_options = { from: ENV['ADMIN_EMAIL'] }
   end
 end
